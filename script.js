c12f1fe250bb1cb27f6e2ee189921f4f368424a4
Falak Fatma <falak510fatma@gmail.com>
2022-11-05 15:13:02 +0530

    Added files

---
+++ script.js
@@ -0,0 +1,299 @@
+let jokes = [`Why did an old man fall in a well
+Because he couldnt see that well`,
+
+    `Why did the actor fall through the floorboards?
+They were going through a stage!`,
+    `Why did a scarecrow win a Nobel prize?
+He was outstanding in his field!`,
+    `Why are peppers the best at archery?
+Because they habanero!`,
+    `What did the duck say after she bought chapstick?
+Put it on my bill!`,
+    `What do you call a fake noodle?
+An impasta!`,
+    `What did the three-legged dog say when he walked into a saloon?
+“I am looking for the man who shot my paw!”`,
+    `How do you tell the difference between a bull and a cow?
+It is either one or the udder!`,
+    `What's red and smells like blue paint
+Red paint`,
+    `What's the difference between a hippo and a Zippo?
+One is very heavy, the other is a little lighter!
+Advertisement - Continue Reading Below,
+`,
+    `What do you call an illegally parked frog?
+Toad!`,
+    `Why cant you send a duck to space?
+Because the bill would be astronomical!`,
+    `What does Jeff Bezos do before he goes to sleep?
+He puts his PJ-Amazon!`,
+
+    `What happened when the world's tongue-twister champion got arrested?
+They gave him a tough sentence!`,
+
+    `What did the mama cow say to the calf?
+It's pasture bedtime`
+    ,
+    `How does a vampire start a letter?
+Tomb it may concern`,
+
+    `What did one plate say to the other?
+Dinner is on me!   `,
+
+    `Why do hummingbirds hum?
+Because they dont know the words!`
+    ,
+    `What do sprinters eat before a race?
+Nothing. They fast!`,
+
+    `Two muffins are baking in an oven. One of them looks to the other and says, "Phew, it's getting hot in here!" The other looks back and says, "Ack! A talking muffin!`,
+    `
+Animal Jokes for Kids
+best jokes for kids`,
+    `What kind of dog does a magician have?
+A Labracadabrad`
+    ,
+    ` do you call an elephant that doesnt matter?
+An irrelephant!`,
+
+    `What do you get when you cross a rabbit with shellfish?
+An oyster bunny!`,
+
+    `Where do polar bears keep their money?
+In a snow bank!`,
+    `
+Why did the pony get sent to his room?
+He wouldnt stop horsing around! `,
+    `
+Where do cows go on Friday nights?
+They go to the moo-vies!`,
+    `    Yo Mama so short she has to hold a sign up that says, "Don't spit, I can't swim  `,
+
+    `   Yo Mama so small she has to slam-dunk her bus fare  `,
+
+    `    Yo Mama so old she rode dinosaurs to school  `,
+
+    `    Yo Mama so old her memory is in black and white  `,
+
+    `    Why didn't the quarter roll down the hill with the nickel?
+Because it had more cents  `,
+
+    `   Why is the obtuse triangle always so frustrated?
+Because it’s never right  `,
+
+    `    Why is six afraid of seven?
+Because seven eight nine  `,
+
+    `    Why was the equal sign so humble?
+Because he wasn’t greater than or less than anyone else  `,
+
+    `    What do you call guys who love math?
+Algebros  `,
+
+    `    How do you stay warm in any room?
+Go to the corner—it’s always 9  degrees  `,
+
+    `    Why was the fraction nervous about marrying the decimal?
+Because he would have to convert  `,
+
+    `1   Are monsters good at math?
+Not unless you count Dracula  `,
+
+    `    Why was the math book sad?
+Because it had too many problems  `,
+
+    `    Why does nobody talk to circles?
+Because there’s no point  `,
+
+    `    What do you get from a pampered cow?
+Spoiled milk  `,
+
+
+    `   Why did the snake cross the road?
+To get to the other ssside  `,
+
+    `  Why are fish so smart?
+Because they live in schools  `,
+
+
+    `   What did the ocean say to the pirate?
+Nothing, it just waved  
+`,
+    `   How does a vampire start a letter?
+Tomb it may concern   `,
+
+    `   What is a monster’s favorite dessert?
+I scream  `,
+
+    `   What monster plays tricks on Halloween?
+Prank-enstein  `,
+
+    `  What kind of music do mummies love?
+Wrap music   `,
+
+    `   What fruit do scarecrows love the most?
+Straw-berries   `,
+
+    ` What does a witch use to do her hair?
+Scarespray   `,
+
+    `    What room does a ghost not need?
+A living room   `,
+
+    `   What kind of dog does Dracula have?
+A blood hound  `,
+
+    `   What is a ghost’s nose full of?
+Boo-gers  `,
+
+    `   What do birds say on Halloween?
+Trick or tweet  `,
+
+    `   Are black cats bad luck?
+Sure, if you’re a mouse  `,
+
+    `   How do you fix a cracked pumpkin?
+A pumpkin patch  `,
+
+    `    When is it bad luck to be followed by a black cat?
+When you’re a mouse  `,
+
+    `    What do you call two witches living together?
+Broommates`,
+
+    `    What happens when a vampire goes in the snow?
+Frost bite  `,
+    `   Yo Mama so small she has to slam-dunk her bus fare  `,
+
+    `    Yo Mama so old she rode dinosaurs to school  `,
+
+    `    Yo Mama so old her memory is in black and white  `,
+
+    `    Why didn't the quarter roll down the hill with the nickel?
+Because it had more cents  `,
+
+    `   Why is the obtuse triangle always so frustrated?
+Because it’s never right  `,
+
+    `    Why is six afraid of seven?
+Because seven eight nine  `,
+
+    `    Why was the equal sign so humble?
+Because he wasn’t greater than or less than anyone else  `,
+
+    `    What do you call guys who love math?
+Algebros  `,
+
+    `    How do you stay warm in any room?
+Go to the corner—it’s always 9  degrees  `,
+
+    `    Why was the fraction nervous about marrying the decimal?
+Because he would have to convert  `,
+
+    `1   Are monsters good at math?
+Not unless you count Dracula  `,
+
+    `    Why was the math book sad?
+Because it had too many problems  `,
+
+    `    Why does nobody talk to circles?
+Because there’s no point  `,
+
+    `    What do you get from a pampered cow?
+Spoiled milk  `,
+
+
+    `   Why did the snake cross the road?
+To get to the other ssside  `,
+
+    `  Why are fish so smart?
+Because they live in schools  `,
+
+
+    `   What did the ocean say to the pirate?
+Nothing, it just waved  
+`,
+    `   How does a vampire start a letter?
+Tomb it may concern   `,
+
+    `   What is a monster’s favorite dessert?
+I scream  `,
+    ,
+
+    `Why did the actor fall through the floorboards?
+They were going through a stage!`,
+    `Why did a scarecrow win a Nobel prize?
+He was outstanding in his field!`,
+    `Why are peppers the best at archery?
+Because they habanero!`,
+    `What did the duck say after she bought chapstick?
+Put it on my bill!`,
+    `What do you call a fake noodle?
+An impasta!`,
+    `What did the three-legged dog say when he walked into a saloon?
+“I am looking for the man who shot my paw!”`,
+    `How do you tell the difference between a bull and a cow?
+It is either one or the udder!`,
+    `What's red and smells like blue paint
+Red paint`,
+    `What's the difference between a hippo and a Zippo?
+One is very heavy, the other is a little lighter!
+Advertisement - Continue Reading Below,
+`,
+    `What do you call an illegally parked frog?
+Toad!`,
+    `Why cant you send a duck to space?
+Because the bill would be astronomical!`,
+    `What does Jeff Bezos do before he goes to sleep?
+He puts his PJ-Amazon!`,
+
+    `What happened when the world's tongue-twister champion got arrested?
+They gave him a tough sentence!`,
+
+    `What did the mama cow say to the calf?
+It's pasture bedtime`
+    ,
+    `How does a vampire start a letter?
+Tomb it may concern`,
+
+    `What did one plate say to the other?
+Dinner is on me!   `,
+
+    `Why do hummingbirds hum?
+Because they dont know the words!`
+    ,
+    `What do sprinters eat before a race?
+Nothing. They fast!`,
+
+    `Two muffins are baking in an oven. One of them looks to the other and says, "Phew, it's getting hot in here!" The other looks back and says, "Ack! A talking muffin!`,
+    `
+Animal Jokes for Kids
+best jokes for kids`,
+    `What kind of dog does a magician have?
+A Labracadabrad`
+    ,
+    ` do you call an elephant that doesnt matter?
+An irrelephant!`,
+
+    `What do you get when you cross a rabbit with shellfish?
+An oyster bunny!`,
+
+    `Where do polar bears keep their money?
+In a snow bank!`,
+    `
+Why did the pony get sent to his room?
+He wouldnt stop horsing around! `,]
+let btn = document.getElementById('btn')
+// console.log(text)
+// console.log(btn)
+// console.log(jokes.length)
+let randomJokes = parseInt(Math.random() * 100)
+let genetate = jokes[randomJokes]
+// console.log(genetate)
+
+btn.addEventListener("click", myFunction)
+// btn.on("click",myFunction())
+function myFunction() {
+    let text = document.getElementById('text')
+    text.innerHTML=`${genetate} `
+}
